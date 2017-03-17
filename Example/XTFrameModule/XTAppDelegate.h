e4b2cbcad0271fe3873a2331958d996892a254b9
Zhaoxt <zhaoxt@tritonsfs.com>
2017-03-17 14:41:22 +0800

    Initial Commit

---
+++ Example/XTFrameModule/XTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XTAppDelegate.h
+//  XTFrameModule
+//
+//  Created by BirdandLion on 03/17/2017.
+//  Copyright (c) 2017 BirdandLion. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/XTFrameModule/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XTFrameModule
+//
+//  Created by BirdandLion on 03/17/2017.
+//  Copyright (c) 2017 BirdandLion. All rights reserved.
+//
+
+@import UIKit;
+#import "XTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XTAppDelegate class]));
+    }
+}
